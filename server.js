a641ff944896f9aa2a708cf95bb4131718ff91b0
ptr <putrasananda61@gmail.com>
2026-05-11 23:20:56 +0700

    fix: downgrade Express to v4, simplify server.js

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname)));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.listen(PORT, () => {
   console.log(`Frontend server running on port ${PORT}`);
 });
